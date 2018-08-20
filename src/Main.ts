14ebe0231693fcd597e670caed45034e0b8c335e
Yuancheng Zhang <old.far.hot@gmail.com>
2018-08-20 18:56:35 +0800

    remove ussless components, increase bird size

--- src/Main.ts
+++ src/Main.ts
@@ -63,7 +63,6 @@ class Main extends egret.DisplayObjectContainer {
         await this.loadResource()
         this.createGameScene();
         const result = await RES.getResAsync("description_json")
-        this.startAnimation(result);
         await platform.login();
         const userInfo = await platform.getUserInfo();
         console.log(userInfo);
@@ -96,48 +95,6 @@ class Main extends egret.DisplayObjectContainer {
         sky.width = stageW;
         sky.height = stageH;
 
-        let topMask = new egret.Shape();
-        topMask.graphics.beginFill(0x000000, 0.5);
-        topMask.graphics.drawRect(0, 0, stageW, 172);
-        topMask.graphics.endFill();
-        topMask.y = 33;
-        this.addChild(topMask);
-
-        let icon = this.createBitmapByName("egret_icon_png");
-        this.addChild(icon);
-        icon.x = 26;
-        icon.y = 33;
-
-        let line = new egret.Shape();
-        line.graphics.lineStyle(2, 0xffffff);
-        line.graphics.moveTo(0, 0);
-        line.graphics.lineTo(0, 117);
-        line.graphics.endFill();
-        line.x = 172;
-        line.y = 61;
-        this.addChild(line);
-
-        let colorLabel = new egret.TextField();
-        colorLabel.textColor = 0xffffff;
-        colorLabel.width = stageW - 172;
-        colorLabel.textAlign = "center";
-        colorLabel.text = "Hello Egret";
-        colorLabel.size = 24;
-        colorLabel.x = 172;
-        colorLabel.y = 80;
-        this.addChild(colorLabel);
-
-        let textfield = new egret.TextField();
-        this.addChild(textfield);
-        textfield.alpha = 0;
-        textfield.width = stageW - 172;
-        textfield.textAlign = egret.HorizontalAlign.CENTER;
-        textfield.size = 24;
-        textfield.textColor = 0xffffff;
-        textfield.x = 172;
-        textfield.y = 135;
-        this.textfield = textfield;
-
         let birdSkeleton = RES.getRes("bird_ske_json");
         let birdTexture = RES.getRes("bird_tex_png")
         let birdTextureData = RES.getRes("bird_tex_json");
@@ -148,8 +105,8 @@ class Main extends egret.DisplayObjectContainer {
         this.addChild(birdAnim);
         birdAnim.x = stageW * .5;
         birdAnim.y = stageH * .5;
-        birdAnim.scaleX = 2;
-        birdAnim.scaleY = 2;
+        birdAnim.scaleX = 3;
+        birdAnim.scaleY = 3;
         birdAnim.animation.play("fly");
     }
 
@@ -162,37 +119,5 @@ class Main extends egret.DisplayObjectContainer {
         let texture: egret.Texture = RES.getRes(name);
         result.texture = texture;
         return result;
-    }
-
-    /**
-     * 描述文件加载成功，开始播放动画
-     * Description file loading is successful, start to play the animation
-     */
-    private startAnimation(result: string[]) {
-        let parser = new egret.HtmlTextParser();
-
-        let textflowArr = result.map(text => parser.parse(text));
-        let textfield = this.textfield;
-        let count = -1;
-        let change = () => {
-            count++;
-            if (count >= textflowArr.length) {
-                count = 0;
-            }
-            let textFlow = textflowArr[count];
-
-            // 切换描述内容
-            // Switch to described content
-            textfield.textFlow = textFlow;
-            let tw = egret.Tween.get(textfield);
-            tw.to({ "alpha": 1 }, 200);
-            tw.wait(2000);
-            tw.to({ "alpha": 0 }, 200);
-            tw.call(change, this);
-        };
-
-        change();
-
-
     }
 }
